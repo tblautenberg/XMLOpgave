f779a1b6002d26f3988786912f6ba3e85f4b5c1c
Tobias Blautenberg <tobbl01@eamv.dk>
2024-09-01 22:31:39 +0200

    Add project files.

---
+++ CursedShit/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CursedShit
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person() {
+            Name = String.Empty;
+            Age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public String ToXML()
+        {
+            using (StringWriter sw = new StringWriter()) 
+            {
+                using (XmlWriter writer = XmlWriter.Create(sw))
+                {
+                    writer.WriteStartElement("person");
+                    writer.WriteElementString("name", Name);
+                    writer.WriteElementString("age", Age.ToString());
+                    writer.WriteEndElement();
+                }
+
+                return sw.ToString();
+            }
+        }
+
+        public void FromXML(string xml)
+        {
+            using (StringReader sr = new StringReader(xml)) 
+            {
+                using (XmlReader reader = XmlReader.Create(sr)) 
+                {
+                    reader.ReadToFollowing("person");
+                    reader.ReadToFollowing("name");
+                    Name = reader.ReadElementContentAsString();
+                    reader.ReadToFollowing("age");
+                    Age = reader.ReadElementContentAsInt();
+                }
+            }
+        }
+    }
+}
---
+++ CursedShit/Program.cs
@@ -0,0 +1,18 @@
+﻿using CursedShit;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person Tobias = new Person("Tobias", 30);
+        Person Weronika = new Person();
+
+        String TobiasLol = Tobias.ToXML();
+
+        Weronika.FromXML(TobiasLol);
+
+        Console.WriteLine(Weronika.Age);
+
+
+    }
+}
